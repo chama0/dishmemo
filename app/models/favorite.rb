f2fbda0178de374d8fc09ff0fc1c731671050685
chama0 <chama_zer0@yahoo.co.jp>
2021-06-09 13:58:09 +0900

    rubocopによるコード自動修正

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -3,5 +3,5 @@ class Favorite < ApplicationRecord
   belongs_to :dish
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
-  validates :dish_id, presence: true 
+  validates :dish_id, presence: true
 end
