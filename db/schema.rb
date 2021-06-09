bf64c8aaa39c2f5cbf2c945f81a94d9b9ef57ad7
chama0 <chama_zer0@yahoo.co.jp>
2021-06-09 16:07:09 +0900

    Userモデルにnotificationカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_09_063739) do
+ActiveRecord::Schema.define(version: 2021_06_09_070358) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "dish_id"
@@ -78,6 +78,7 @@ ActiveRecord::Schema.define(version: 2021_06_09_063739) do
     t.string "password_digest"
     t.string "remember_digest"
     t.boolean "admin", default: false
+    t.boolean "notification", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
