496fbc6831e6aff73b38cd241ef1149283100335
chama0 <chama_zer0@yahoo.co.jp>
2021-07-21 21:15:19 +0900

    料理へのコメントが降順になるよう修正

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :dish
+  default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
   validates :dish_id, presence: true
   validates :content, presence: true, length: { maximum: 50 }
